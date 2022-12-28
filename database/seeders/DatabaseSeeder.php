b09d168c23def807f6e2ea3de2c19372e939de59
rf-ferreira <r4fael.s0uza@gmail.com>
2022-12-28 17:02:16 -0300

    feat: add db seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->has(Book::factory(3))->create();
+        $user->booksRented()->attach(1);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+        User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@email.com',
+        ]);
     }
 }
