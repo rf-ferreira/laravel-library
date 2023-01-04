92d7f6dad0f007d24f30bf6c977d84619ed23e66
rf-ferreira <r4fael.s0uza@gmail.com>
2023-01-04 17:44:42 -0300

    feat: add css for main layout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <title>Library</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="nav-links">
+                <a href="" class="nav-selected">Home</a>
+                <a href="">Books</a>
+                <a href="">Login</a>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
